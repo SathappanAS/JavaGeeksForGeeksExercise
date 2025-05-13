6a74359ad7427e6fd60f7e1ee74e52b553700960
Sathappan <sathappanas@outlook.com>
2025-05-12 21:39:13 -0700

    ArrayTraversal

---
+++ src/ArraysPracticeQuestions/ArrayTraversal.java
@@ -0,0 +1,19 @@
+package ArraysPracticeQuestions;
+
+public class ArrayTraversal {
+	
+	public static void arrayTraversal(int a[]) {
+		for(int i=0;i<a.length;i++) {
+			System.out.print(a[i] + " ");
+		}
+	
+	}
+	
+	public static void main(String[] args) {
+		
+		int a[]= {54, 43, 2, 1, 5};
+		ArrayTraversal.arrayTraversal(a);
+
+	}
+
+}
