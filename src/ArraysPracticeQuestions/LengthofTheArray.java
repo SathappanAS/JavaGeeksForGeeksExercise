817bb463aef31dfbddfef84079bf7ff7bcb7a3f3
Sathappan <sathappanas@outlook.com>
2025-05-12 21:34:46 -0700

    LengthofAnArray

---
+++ src/ArraysPracticeQuestions/LengthofTheArray.java
@@ -0,0 +1,20 @@
+package ArraysPracticeQuestions;
+
+public class LengthofTheArray {
+
+	
+	public static int arrayLength(int[] arr) {
+	
+		int a = arr.length;
+		return a;
+	}
+	
+	public static void main(String[] args) {
+		
+		int arr [] = {54, 43, 2, 1, 5};
+		
+		System.out.println(LengthofTheArray.arrayLength(arr));
+		
+	}
+
+}
