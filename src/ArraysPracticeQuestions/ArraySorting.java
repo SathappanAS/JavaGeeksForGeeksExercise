f672b8c272dabfd79404853f8337eb23f535bd22
Sathappan <sathappanas@outlook.com>
2025-05-12 21:53:17 -0700

    ArraySorting

--- src/ArraysPracticeQuestions/ArraySorting.java
+++ src/ArraysPracticeQuestions/ArraySorting.java
@@ -1,28 +1,27 @@
 package ArraysPracticeQuestions;
 
 public class ArraySorting {
-	
-	  public static boolean arraySortedOrNot(int[] arr) {
-		  
-		  
-		  int  n = arr.length;
-	         for (int i = 0; i<n-1; i++){
-	             if(arr[i]>arr[i+1]){
-	                 return false;
-	             }
-		  
-	         }
-		  return true;
-	         
-	    }
+
+	public static boolean arraySortedOrNot(int[] arr) {
+
+		int n = arr.length;
+		for (int i = 0; i < n - 1; i++) {
+			if (arr[i] > arr[i + 1]) {
+				return false;
+			}
+
+		}
+		return true;
+
+	}
 
 	public static void main(String[] args) {
-	int s[]= {90,80,100,70,40,30};
-	int b[]= {10,20,30,40,50};
-	
-	System.out.println(ArraySorting.arraySortedOrNot(s));
-	
-	System.out.println(ArraySorting.arraySortedOrNot(b));
+		int s[] = { 90, 80, 100, 70, 40, 30 };
+		int b[] = { 10, 20, 30, 40, 50 };
+
+		System.out.println(ArraySorting.arraySortedOrNot(s));
+
+		System.out.println(ArraySorting.arraySortedOrNot(b));
 
 	}
 
